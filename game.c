6bebd7545bbb27f1f0ba2cd858937220fe97bac5
João Pedro Licks Corso <jp.corso123@gmail.com>
2022-04-03 14:53:16 -0300

    Correção do bug das setas(x e y) e rotação do vetor, mais a adição de tijolos

--- game.c
+++ game.c
@@ -1,7 +1,5 @@
 #include "raylib.h"
 
-#define VELOCITY    20.0f
-
 int main(void){
 
     // Initialization
@@ -11,13 +9,14 @@ int main(void){
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    Vector2 tankPosition = {(float)screenWidth/2, (float)screenHeight/2}; //posição inicial do tanque
+    Vector2 tankPosition = {(float)screenWidth/2.0f, (float)screenHeight/2.0f}; //posição inicial do tanque
 
     Vector2 size = {(float)20, (float)20}; //tamanho do retangulo
 
     Texture2D tank = LoadTexture("./public/tank.png");
     Texture2D tijolo = LoadTexture("./public/tijolo.png");
     Texture2D coluna_tijolo = LoadTexture("./public/coluna_tijolo.png");
+    Texture2D linha_tijolo = LoadTexture("./public/linha_tijolo.png");
 
     SetTargetFPS(60);
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -27,31 +26,75 @@ int main(void){
     {
        
 
-        if (IsKeyDown(KEY_RIGHT)) tankPosition.x += 3.5f;
-        if (IsKeyDown(KEY_LEFT)) tankPosition.x -= 3.5f;
-        if (IsKeyDown(KEY_UP)) tankPosition.y -= 3.5f;
-        if (IsKeyDown(KEY_DOWN)) tankPosition.y += 3.5f;
-
+        
         // Draw
         //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         BeginDrawing();
 
             ClearBackground(BLACK);
 
+        //texturas dos tijolos
+
+        //escada de tijolos
         DrawTexture(tijolo, 200, 100, RAYWHITE);
         DrawTexture(tijolo, 230, 120, RAYWHITE);
+        DrawTexture(tijolo, 260, 140, RAYWHITE);
+        DrawTexture(tijolo, 290, 160, RAYWHITE);
 
-
-
+        DrawTexture(tijolo, 100, 270, RAYWHITE);
         DrawTexture(tijolo, 20, 50, RAYWHITE);
+
         DrawTexture(coluna_tijolo, 20, 100, RAYWHITE);
         DrawTexture(coluna_tijolo, 90, 100, RAYWHITE);
         DrawTexture(tijolo, 70, 300, RAYWHITE);
 
-        DrawTextureV(tank, tankPosition, GREEN);
+        //"U" de tijolos
+        DrawTexture(linha_tijolo, 500, 400, RAYWHITE);
+        DrawTexture(coluna_tijolo, 500, 400, RAYWHITE);
+        DrawTexture(coluna_tijolo, 600, 400, RAYWHITE);
 
-        Rectangle limite = {0, 0, 780, 580};
+        DrawTexture(linha_tijolo, 400, 90, RAYWHITE);
+        DrawTexture(linha_tijolo, 90, 400, RAYWHITE);
+        DrawTexture(coluna_tijolo, 10, 400, RAYWHITE);
+        DrawTexture(linha_tijolo, 600, 200, RAYWHITE);
+        DrawTexture(coluna_tijolo, 600, 200, RAYWHITE);
+        DrawTexture(coluna_tijolo, 400, 250, RAYWHITE);
 
+
+        //movimento das setas e sentido/direção do tanque
+        if(IsKeyDown(KEY_RIGHT)){
+            DrawTextureEx(tank, tankPosition,270, 1, GREEN);
+            tankPosition.x += 3.5f; 
+        }
+        else{
+            if(IsKeyDown(KEY_LEFT)){
+                DrawTextureEx(tank, tankPosition,90, 1, GREEN);
+                tankPosition.x -= 3.5f;
+            }
+            else{
+                if(IsKeyDown(KEY_UP)){
+                    DrawTextureEx(tank, tankPosition,360, 1, GREEN);
+                    tankPosition.y -= 3.5f;
+                }
+                else{
+                    if(IsKeyDown(KEY_DOWN)){
+                        DrawTextureEx(tank, tankPosition,180, 1, GREEN);
+                        tankPosition.y += 3.5f;
+                    }
+                    else{
+                        DrawTextureEx(tank, tankPosition,270, 1, GREEN);
+                    }
+                }
+            }
+        }
+
+        //Retangulo do limite para tanque não sair da tela
+        Rectangle limite = {0, 0, 760, 560};
+
+        /*if ((tankPosition.x >= (GetScreenWidth() - 10)) || (tankPosition.x <= 10)) tankPosition.x *= -1.0f;
+        if ((tankPosition.y >= (GetScreenHeight() - 10)) || (tankPosition.y <= 10)) tankPosition.y *= -1.0f;*/
+
+        //Checa colisão entre tanque e retangulo do limite
         if (CheckCollisionPointRec(tankPosition, limite))
         {
 
@@ -60,6 +103,7 @@ int main(void){
             tankPosition.x *= -1;
             tankPosition.y *= -1;
         }
+
         
         EndDrawing();
         //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
